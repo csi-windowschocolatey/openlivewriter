141848ebb43ec8e4e11e5ef92e1ff5677c1c5ce3
DarwinJS <DarwinJS@sanoys.com>
2015-12-10 09:37:47 -0500

    first commit

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿
+$ErrorActionPreference = 'Stop';
+
+$packageName= 'openlivewriter'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://openlivewriter.azureedge.net/stable/Releases/Setup.exe'
+$url64      = 'https://openlivewriter.azureedge.net/stable/Releases/Setup.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  url64bit      = $url64
+
+  silentArgs    = '--silent'
+}
+
+Install-ChocolateyPackage @packageArgs
+
+Write-Output "The install log is at ``"$env:localappdata\SquirrelTemp\SquirrelSetup.log`""
